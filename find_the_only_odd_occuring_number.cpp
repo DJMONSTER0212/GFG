5ea16c2ee61fbd8f0e89f23dd8b6189caacfe624
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-03-10 13:06:23 +0530

    Update find_the_only_odd_occuring_number.cpp

--- find_the_only_odd_occuring_number.cpp
+++ find_the_only_odd_occuring_number.cpp
@@ -5,7 +5,7 @@ int main(){
     cin>>n;
     int a[n];
     int sum=0;
-    for(int i=0;i<n;i++){cin>>a[i];`
+    for(int i=0;i<n;i++){cin>>a[i];
         sum+= a[i];
     }
     for(int i=0;i<n;i++){
